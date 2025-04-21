2b9338081962c1e7bcb23ab47b8a44b4b0e85572
aminammar1 <ammar.mohamdamine@gmail.com>
2025-04-21 18:30:45 +0100

    fix indentation and import path for SignIn component in SignInPage

--- client/app/(auth)/sign-in/page.jsx
+++ client/app/(auth)/sign-in/page.jsx
@@ -1,13 +1,13 @@
-import Signin from '@/components/modules/SignIn/SignIn'
+import Signin from '@/components/modules/Signin/SignIn'
 import React from 'react'
 
 export default function SignInPage() {
   return (
     <div className="h-full grid grid-cols-1 lg:grid-cols-2">
-        <div className="hidden lg:flex bg-cover bg-center bg-[url('/assets/images/image7.jpg')]"></div>
-        <div className="flex justify-center items-center">
-            <Signin />
-        </div>
+      <div className="hidden lg:flex bg-cover bg-center bg-[url('/assets/images/image7.jpg')]"></div>
+      <div className="flex justify-center items-center">
+        <Signin />
+      </div>
     </div>
   )
 }
